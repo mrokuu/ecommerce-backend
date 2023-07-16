ef4fd84a66c7e4952a2399992013e52b673b4109
mrokuu <mroczkowskiadammm@gmail.com>
2023-07-16 18:27:12 +0200

    adding photo

--- src/main/java/com/app/ecommerce/admin/product/controller/AdminProductController.java
+++ src/main/java/com/app/ecommerce/admin/product/controller/AdminProductController.java
@@ -1,15 +1,25 @@
 package com.app.ecommerce.admin.product.controller;
 
 import com.app.ecommerce.admin.product.controller.dto.AdminProductDto;
+import com.app.ecommerce.admin.product.controller.dto.UploadResponse;
 import com.app.ecommerce.admin.product.model.AdminProduct;
+import com.app.ecommerce.admin.product.service.AdminProductImageService;
 import com.app.ecommerce.admin.product.service.AdminProductService;
+import com.github.slugify.Slugify;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static com.app.ecommerce.admin.product.mapper.AdminProductMapper.mapAdminProduct;
 
@@ -19,6 +29,7 @@ public class AdminProductController {
 
     public static final Long EMPTY_ID = null;
     private final AdminProductService adminProductService;
+    private final AdminProductImageService productImageService;
 
     @GetMapping("/admin/products")
     public Page<AdminProduct> getProducts(Pageable pageable){
@@ -48,6 +59,25 @@ public class AdminProductController {
     @DeleteMapping("/admin/products/{id}")
     public void deleteProduct(@PathVariable Long id){
         adminProductService.deleteProduct(id);
+    }
+
+
+    @PostMapping("/admin/products/upload-image")
+    public UploadResponse uploadImage(@RequestParam("file") MultipartFile multipartFile){
+        try(InputStream inputStream = multipartFile.getInputStream()) {
+            String savedFileName = productImageService.uploadImage(multipartFile.getOriginalFilename(), inputStream);
+            return new UploadResponse(savedFileName);
+        } catch (IOException e) {
+            throw new RuntimeException("Coś poszło źle podczas wgrywania pliku", e);
+        }
+    }
+
+    @GetMapping("/data/productImage/{filename}")
+    public ResponseEntity<Resource> serveFiles(@PathVariable String filename) throws IOException {
+        Resource file = productImageService.serveFiles(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, Files.probeContentType(Path.of(filename)))
+                .body(file);
     }
 
 
---
+++ src/main/java/com/app/ecommerce/admin/product/controller/dto/UploadResponse.java
@@ -0,0 +1,4 @@
+package com.app.ecommerce.admin.product.controller.dto;
+
+public record UploadResponse(String filename) {
+}
