9522c032be5f04e41412c98e0fb21dce1aba817b
mrokuu <mroczkowskiadammm@gmail.com>
2023-07-21 14:34:26 +0200

    email

--- src/main/resources/database/2023-07-21/01-create-order.sql
+++ src/main/resources/database/2023-07-21/01-create-order.sql
@@ -5,7 +5,14 @@ create table `order`(
                         id bigint not null auto_increment PRIMARY KEY,
                         placeDate datetime not null,
                         orderStatus varchar(32) not null,
-                        grossValue decimal(6,2) not null
+                        grossValue decimal(6,2) not null,
+                        firstname varchar(64) not null,
+                        lastname varchar(64) not null,
+                        street varchar(80) not null,
+                        zipcode varchar(10) not null,
+                        city varchar(64) not null,
+                        email varchar(64) not null,
+                        phone varchar(16) not null,
 );
 create table order_row(
                           id bigint not null auto_increment PRIMARY KEY,
