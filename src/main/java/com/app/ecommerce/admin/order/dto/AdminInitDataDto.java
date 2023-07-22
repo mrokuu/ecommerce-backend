63b308ec8f2bc801a9d40088212a3e4ff022a19b
mrokuu <mroczkowskiadammm@gmail.com>
2023-07-22 15:15:47 +0200

    admin order

--- src/main/java/com/app/ecommerce/admin/order/dto/AdminInitDataDto.java
+++ src/main/java/com/app/ecommerce/admin/order/dto/AdminInitDataDto.java
@@ -1,4 +1,13 @@
 package com.app.ecommerce.admin.order.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
 public class AdminInitDataDto {
+    private Map<String, String> orderStatuses;
 }
+
--- src/main/java/com/app/ecommerce/admin/order/dto/AdminOrderStats.java
+++ src/main/java/com/app/ecommerce/admin/order/dto/AdminOrderStats.java
@@ -1,4 +1,18 @@
 package com.app.ecommerce.admin.order.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Builder
 public class AdminOrderStats {
+    private List<Integer> label;
+    private List<BigDecimal> sale;
+    private List<Long> order;
+    private Long ordersCount;
+    private BigDecimal salesSum;
 }
+
